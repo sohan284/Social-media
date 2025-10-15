4fb7006925330211c4516b0160b0264184047c65
Nafiz Al Tuarbi <nafizalturabi18cse@gmail.com>
2025-10-15 20:29:06 +0600

    Navigation link added

---
+++ components/Icons/HexagonIcon.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const HexagonIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M12.0001 2L20.6601 7V17L12.0001 22L3.34009 17V7L12.0001 2Z"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="square"
+      />
+      <path
+        d="M16 12C16 13.0609 15.5786 14.0783 14.8284 14.8284C14.0783 15.5786 13.0609 16 12 16C10.9391 16 9.92172 15.5786 9.17157 14.8284C8.42143 14.0783 8 13.0609 8 12C8 10.9391 8.42143 9.92172 9.17157 9.17157C9.92172 8.42143 10.9391 8 12 8C13.0609 8 14.0783 8.42143 14.8284 9.17157C15.5786 9.92172 16 10.9391 16 12Z"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="square"
+      />
+    </svg>
+  );
+};
+
+export default HexagonIcon;
--- components/Layouts/HomeLayout.tsx
+++ components/Layouts/HomeLayout.tsx
@@ -1,8 +1,39 @@
 "use client";
 import React, { useState } from "react";
-import { FiMenu, FiX } from "react-icons/fi";
+import {FiMenu, FiPlus, FiX } from "react-icons/fi";
 import Navbar from "../Shared/Navbar/Navbar";
 import bg from "../../public/main-bg.jpg";
+import Link from "next/link";
+import { GoHome } from "react-icons/go";
+import FriendsIcon from "../Icons/FriendsIcon";
+import { IoTrendingUp } from "react-icons/io5";
+import StoreIcon from "../Icons/StoreIcon";
+import HexagonIcon from "../Icons/HexagonIcon";
+
+const menuItems = [
+  {
+    icon: <GoHome size={24} />,
+    label: "Home",
+    href: "/",
+  },
+  {
+    icon: <FriendsIcon />,
+    label: "Friends",
+    href: "/",
+  },
+  {
+    icon: <IoTrendingUp size={24} />,
+    label: "Popular",
+    href: "/",
+  },
+  {
+    icon: <StoreIcon />,
+    label: "Virtual Store",
+    href: "/",
+  },
+
+];
+
 
 export default function HomeLayout({
   children,
@@ -32,7 +63,21 @@ export default function HomeLayout({
           </div>
 
           <nav className="space-y-3 text-gray-700 mt-16">
-            <div className="bg-[#06133FBF] backdrop-blur-[1px] p-6 rounded-2xl">
+            <div className="bg-[#06133FBF] backdrop-blur-[1px] py-6 px-2 rounded-2xl">
+              {menuItems.map((item) => (
+                <Link href={item.href} className="text-xs text-white flex items-center gap-5 hover:bg-[#06133FBF] p-2.5 px-6 rounded-xl duration-300 ease-in-out">
+                  {item.icon} {item.label}
+                </Link>
+              ))}
+            </div>
+            <div className="bg-[#06133FBF] backdrop-blur-[1px] py-1.5 px-2.5 rounded-2xl">
+              <h3 className="text-base text-[#BCB3B3] px-8 mb-7">Communities</h3>
+              <button className="w-full text-xs text-white flex items-center gap-5 hover:bg-[#06133FBF] p-2.5 px-6 rounded-xl duration-300 ease-in-out cursor-pointer">
+                <FiPlus size={24} /> Create Communities
+              </button>
+              <button className="w-full text-xs text-white flex items-center gap-5 hover:bg-[#06133FBF] p-2.5 px-6 rounded-xl duration-300 ease-in-out cursor-pointer">
+                <HexagonIcon /> Manage Communities
+              </button>
             </div>
           </nav>
         </aside>
