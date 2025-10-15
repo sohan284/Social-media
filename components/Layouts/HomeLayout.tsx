aec50cdb659f36a562cfef88c6be9503139622b7
Nafiz Al Tuarbi <nafizalturabi18cse@gmail.com>
2025-10-15 21:22:12 +0600

    Sidenav seperated from layout

--- components/Layouts/HomeLayout.tsx
+++ components/Layouts/HomeLayout.tsx
@@ -1,59 +1,32 @@
 "use client";
 import React, { useState } from "react";
-import {FiMenu, FiPlus, FiX } from "react-icons/fi";
+import { FiX } from "react-icons/fi";
 import Navbar from "../Shared/Navbar/Navbar";
 import bg from "../../public/main-bg.jpg";
-import Link from "next/link";
-import { GoHome } from "react-icons/go";
-import FriendsIcon from "../Icons/FriendsIcon";
-import { IoTrendingUp } from "react-icons/io5";
-import StoreIcon from "../Icons/StoreIcon";
-import HexagonIcon from "../Icons/HexagonIcon";
-
-const menuItems = [
-  {
-    icon: <GoHome size={24} />,
-    label: "Home",
-    href: "/",
-  },
-  {
-    icon: <FriendsIcon />,
-    label: "Friends",
-    href: "/",
-  },
-  {
-    icon: <IoTrendingUp size={24} />,
-    label: "Popular",
-    href: "/",
-  },
-  {
-    icon: <StoreIcon />,
-    label: "Virtual Store",
-    href: "/",
-  },
-
-];
-
+import SidebarNavLink from "../Shared/SidebarNavLink/SidebarNavLink";
 
 export default function HomeLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   return (
-    <div className=" flex flex-col ">
-      {/* Top Navbar (Fixed) */}
+    <div className="flex flex-col">
       <Navbar />
 
-      {/* Main Layout */}
       <div className="flex flex-1">
+        {/* LEFT SIDEBAR */}
         <aside
-          style={{ backgroundImage: `url(${bg.src})` }}
-          className={`fixed hover:overflow-y-auto custom-scroll left-0 h-[100vh] bg-cover bg-center bg-no-repeat shadow-md transform transition-transform duration-300 lg:translate-x-0 z-30 w-[370px] p-10  ${
-            isSidebarOpen ? "translate-x-0" : "-translate-x-full"
-          }`}
+          style={{
+            backgroundImage: `url(${bg.src})`,
+            scrollbarGutter: "stable both-edges",
+          }}
+          className={`fixed left-0 h-[100vh] bg-cover bg-center bg-no-repeat shadow-md transform transition-transform duration-300 lg:translate-x-0 z-30 w-[370px] p-10 
+            ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"} 
+            hover:overflow-y-auto overflow-y-hidden custom-scroll
+          `}
         >
           <div className="flex justify-between items-center mb-4 lg:hidden">
             <h2 className="font-semibold text-lg text-[#022735]">Menu</h2>
@@ -63,26 +36,11 @@ export default function HomeLayout({
           </div>
 
           <nav className="space-y-3 text-gray-700 mt-16">
-            <div className="bg-[#06133FBF] backdrop-blur-[1px] py-6 px-2 rounded-2xl">
-              {menuItems.map((item) => (
-                <Link href={item.href} className="text-xs text-white flex items-center gap-5 hover:bg-[#06133FBF] p-2.5 px-6 rounded-xl duration-300 ease-in-out">
-                  {item.icon} {item.label}
-                </Link>
-              ))}
-            </div>
-            <div className="bg-[#06133FBF] backdrop-blur-[1px] py-1.5 px-2.5 rounded-2xl">
-              <h3 className="text-base text-[#BCB3B3] px-8 mb-7">Communities</h3>
-              <button className="w-full text-xs text-white flex items-center gap-5 hover:bg-[#06133FBF] p-2.5 px-6 rounded-xl duration-300 ease-in-out cursor-pointer">
-                <FiPlus size={24} /> Create Communities
-              </button>
-              <button className="w-full text-xs text-white flex items-center gap-5 hover:bg-[#06133FBF] p-2.5 px-6 rounded-xl duration-300 ease-in-out cursor-pointer">
-                <HexagonIcon /> Manage Communities
-              </button>
-            </div>
+            <SidebarNavLink />
           </nav>
         </aside>
 
-        {/* Overlay for Mobile */}
+        {/* OVERLAY FOR MOBILE */}
         {isSidebarOpen && (
           <div
             className="fixed inset-0 bg-black/40 z-20 lg:hidden"
@@ -90,15 +48,15 @@ export default function HomeLayout({
           />
         )}
 
-        {/* Center Content (Scrollable) */}
+        {/* MAIN CONTENT */}
         <main className="flex-1 h-[calc(100vh)] px-2 lg:px-6 py-4 lg:ml-64 xl:mr-72 overflow-y-auto bg-gradient-to-tl from-[#956194] via-[#b8586e] to-[#956194]">
           <div className="max-w-2xl mx-auto mt-16">{children}</div>
         </main>
 
-        {/* Right Sidebar (Fixed) */}
+        {/* RIGHT SIDEBAR */}
         <aside
           style={{ backgroundImage: `url(${bg.src})` }}
-          className="bg-cover bg-center bg-no-repeat hidden xl:block fixed right-0 w-[370px] h-[calc(100vh)] p-4  shadow-md overflow-y-auto"
+          className="bg-cover bg-center bg-no-repeat hidden xl:block fixed right-0 w-[370px] h-[calc(100vh)] p-4 shadow-md overflow-y-auto"
         >
           <h3 className="font-semibold text-[#022735] mb-3">Trending Today</h3>
           <ul className="space-y-3 text-sm text-gray-700">ggwp will here</ul>
---
+++ components/Shared/SidebarNavLink/SidebarNavLink.tsx
@@ -0,0 +1,113 @@
+import { GoHome } from "react-icons/go";
+import { IoTrendingUp } from "react-icons/io5";
+import { BsExclamationCircle } from "react-icons/bs";
+import { RiMenuSearchLine } from "react-icons/ri";
+import { FaRegQuestionCircle } from "react-icons/fa";
+import FriendsIcon from "../../Icons/FriendsIcon";
+import StoreIcon from "../../Icons/StoreIcon";
+import SubscriptionIcon from "../../Icons/SubscriptionIcon";
+import DatabaseIcon from "../../Icons/DatabaseIcon";
+import Link from "next/link";
+import HexagonIcon from "../../Icons/HexagonIcon";
+import { FiPlus } from "react-icons/fi";
+const menuItems = [
+  {
+    icon: <GoHome size={24} />,
+    label: "Home",
+    href: "/",
+  },
+  {
+    icon: <FriendsIcon />,
+    label: "Friends",
+    href: "/",
+  },
+  {
+    icon: <IoTrendingUp size={24} />,
+    label: "Popular",
+    href: "/",
+  },
+  {
+    icon: <StoreIcon />,
+    label: "Virtual Store",
+    href: "/",
+  },
+];
+const menuItemsTwo = [
+  {
+    icon: <BsExclamationCircle size={24} className="scale-y-[-1]" />,
+    label: "Contact Us",
+    href: "/",
+  },
+  {
+    icon: <RiMenuSearchLine size={24} />,
+    label: "Topics",
+    href: "/",
+  },
+  {
+    icon: <FriendsIcon />,
+    label: "Communities",
+    href: "/",
+  },
+  {
+    icon: <FaRegQuestionCircle size={24} />,
+    label: "Help",
+    href: "/",
+  },
+  {
+    icon: <SubscriptionIcon />,
+    label: "Subscription",
+    href: "/",
+  },
+  {
+    icon: <DatabaseIcon />,
+    label: "Storage",
+    href: "/",
+  },
+];
+
+const SidebarNavLink = () => {
+  return (
+    <nav className="space-y-3 text-gray-700 mt-16">
+      <div className="bg-[#06133FBF] backdrop-blur-[1px] py-6 px-2 rounded-2xl">
+        {menuItems.map((item) => (
+          <Link
+            href={item.href}
+            className="text-xs text-white flex items-center gap-5 hover:bg-[#06133FBF] p-2.5 px-6 rounded-xl duration-300 ease-in-out"
+          >
+            {item.icon} {item.label}
+          </Link>
+        ))}
+      </div>
+      <div className="bg-[#06133FBF] backdrop-blur-[1px] py-1.5 px-2.5 rounded-2xl">
+        <h3 className="text-base text-[#BCB3B3] px-8 mb-7">Communities</h3>
+        <button className="w-full text-xs text-white flex items-center gap-5 hover:bg-[#06133FBF] p-2.5 px-6 rounded-xl duration-300 ease-in-out cursor-pointer">
+          <FiPlus size={24} /> Create Communities
+        </button>
+        <button className="w-full text-xs text-white flex items-center gap-5 hover:bg-[#06133FBF] p-2.5 px-6 rounded-xl duration-300 ease-in-out cursor-pointer">
+          <HexagonIcon /> Manage Communities
+        </button>
+      </div>
+      <div className="bg-[#06133FBF] backdrop-blur-[1px] py-1.5 px-2.5 rounded-2xl">
+        <h3 className="text-base text-[#BCB3B3] px-8 mb-7">Categories</h3>
+        <button className="w-full text-xs text-white flex items-center gap-5 hover:bg-[#06133FBF] p-2.5 px-6 rounded-xl duration-300 ease-in-out cursor-pointer">
+          <FiPlus size={24} /> Join Categories
+        </button>
+        <button className="w-full text-xs text-white flex items-center gap-5 hover:bg-[#06133FBF] p-2.5 px-6 rounded-xl duration-300 ease-in-out cursor-pointer">
+          <HexagonIcon /> Manage Categories
+        </button>
+      </div>
+      <div className="bg-[#06133FBF] backdrop-blur-[1px] py-6 px-2.5 rounded-2xl">
+        {menuItemsTwo.map((item) => (
+          <Link
+            href={item.href}
+            className="text-xs text-white flex items-center gap-5 hover:bg-[#06133FBF] p-2.5 px-6 rounded-xl duration-300 ease-in-out"
+          >
+            {item.icon} {item.label}
+          </Link>
+        ))}
+      </div>
+    </nav>
+  );
+};
+
+export default SidebarNavLink;
