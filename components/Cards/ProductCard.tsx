b96a9bf8195bf45cc77628c20af167f12da457af
hasib1010 <asifmahmud664@gmail.com>
2025-10-25 21:22:40 +0600

    Updated card design

--- components/Cards/ProductCard.tsx
+++ components/Cards/ProductCard.tsx
@@ -11,7 +11,7 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ image = "https://media.istockphoto.com/id/1414801672/photo/cardboard-box-with-cosmetics-product-in-front-od-open-door-buying-online-and-delivery.jpg?s=612x612&w=0&k=20&c=SA9VCzp-QtpzlliX8dM_uoH8K20U1gHqYfsWP08aLl0=", title = "Product Title", price = "$100", rating = 4.5 }) => {
     return (
-        <div className="group relative flex flex-col overflow-hidden rounded-2xl bg-white/90 backdrop-blur-sm shadow-md hover:shadow-xl transition-all duration-300 hover:-translate-y-1 border border-gray-100">
+        <div className="group relative flex flex-col overflow-hidden rounded-2xl bg-white/ backdrop-blur-sm shadow-md hover:shadow-xl transition-all duration-300 hover:-translate-y-1 ">
             {/* Product Image */}
             <div className="relative w-full  overflow-hidden">
                 <img
@@ -26,14 +26,14 @@ const ProductCard: React.FC<ProductCardProps> = ({ image = "https://media.istock
 
             {/* Product Info */}
             <div className="p-4 flex flex-col gap-2">
-                <h3 className="text-lg font-semibold text-gray-800 line-clamp-1">{title}</h3>
+                <h3 className="text-lg font-semibold text-gray-300 line-clamp-1">{title}</h3>
 
                 <div className="flex items-center justify-between">
                     <span className="text-xl font-bold text-indigo-600">{price}</span>
 
                     <div className="flex items-center text-sm text-yellow-500">
                         <FaStar className="w-4 h-4 fill-yellow-400" />
-                        <span className="ml-1 text-gray-700">{rating}</span>
+                        <span className="ml-1 text-gray-400">{rating}</span>
                     </div>
                 </div>
 
--- components/Features/Marketplace/Marketplace/Marketplace.tsx
+++ components/Features/Marketplace/Marketplace/Marketplace.tsx
@@ -3,8 +3,10 @@ import ProductCard from '../../../Cards/ProductCard';
 
 const Marketplace = () => {
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 xl:gap-20 px-4'>
-            <ProductCard />
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6 px-4'>
+            {Array.from({ length: 50 }).map((_, index) => (
+                <ProductCard key={index} />
+            ))}
         </div>
     );
 };
