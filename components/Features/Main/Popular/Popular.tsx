d2415dd4a1cc87ea95c2833e2f737a703ac763ae
hasib1010 <asifmahmud664@gmail.com>
2025-10-26 18:43:48 +0600

    Improved styling and layout for the contact us page

---
+++ components/Features/Main/Popular/Popular.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Popular = () => {
+    return (
+        <div>
+            this is popular page
+        </div>
+    );
+};
+
+export default Popular;
---
+++ src/app/(clinet)/popular/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Popular from '../../../../components/Features/Main/Popular/Popular'
+
+export default function PopularPage() {
+  return (
+    <div><Popular /></div>
+  )
+}
