a492f0613bc7de193f3e679f698db5f5c238739a
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2025-10-30 21:32:35 +0600

    Refined Profile component layout for better responsiveness. Adjusted padding for main content and modified sidebar visibility to hide on smaller screens.

--- components/Features/Profile/Profile.tsx
+++ components/Features/Profile/Profile.tsx
@@ -14,8 +14,8 @@ const Profile = () => {
 
     return (
         <>
-            <div className='max-w-[1220px] mx-auto flex gap-10 pt-6 p-4'>
-                <div className='w-full min-h-[] bg-[#06133F]/75 backdrop-blur-[17.5px] rounded-lg p-10 '>
+            <div className='max-w-[1220px] mx-auto flex gap-10 pt-6 lg:p-4 '>
+                <div className='w-full min-h-[] bg-[#06133F]/75 backdrop-blur-[17.5px] rounded-lg p-4 xl:p-10 '>
                     <div className='border-b border-white/10 pb-4'>
                         <ProfileHeader onAvatarClick={openModal} />
                         <div className='flex items-center gap-2 mt-4 overflow-x-hidden'>
@@ -30,7 +30,7 @@ const Profile = () => {
                         <Post />
                     </div>
                 </div>
-                <div style={{ scrollbarGutter: "stable both-edges" }} className='h-[90vh]  rounded-lg w-[500px] sticky top-18 hover:overflow-y-auto overflow-y-hidden custom-scroll'>
+                <div style={{ scrollbarGutter: "stable both-edges" }} className='h-[90vh]  hidden lg:block rounded-lg w-[500px] sticky top-18 hover:overflow-y-auto overflow-y-hidden custom-scroll'>
                     <ProfileSidebar />
                 </div>
             </div>
