4bf7fd020cc280be9086b924736f9e8ed864ef26
srsohan <sr.sohan088@gmail.com>
2025-12-01 11:05:49 +0600

    add api

--- components/Features/Profile/Profile.tsx
+++ components/Features/Profile/Profile.tsx
@@ -76,7 +76,7 @@ const Profile = () => {
 
   return (
     <>
-      <div className="max-w-[1220px] mx-auto flex gap-10 pt-6 lg:p-4">
+      <div className="mx-auto flex gap-10 pt-6 lg:p-4">
         <div className="w-full bg-[#06133F]/75 backdrop-blur-[17.5px] rounded-lg p-4 xl:p-10">
           <div className="border-b border-white/10 pb-4">
             <ProfileHeader onAvatarClick={openModal} />
